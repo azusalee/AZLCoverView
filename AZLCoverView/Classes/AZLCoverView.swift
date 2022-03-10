4b74ec8861a36a71381742e7e725cce803ec4a33
lizihong <384433472@qq.com>
2022-03-10 11:46:55 +0800

    first commit

---
+++ AZLCoverView/Classes/AZLCoverView.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+/**
+蒙版视图
+ */
+open class AZLCoverView: UIView {
+    
+    /// 镂空路径
+    var hollowPaths: [UIBezierPath] = []
+    
+    /// 添加方形镂空范围
+    /// 
+    /// 镂空部分不处理点击事件(也就是点击镂空部分会穿透到下一层)
+    /// - Parameters:
+    ///   - frame: 镂空范围
+    ///   - inset: 增加上下左右镂空范围
+    public func addHollow(frame: CGRect, inset: UIEdgeInsets = .zero) {
+        let insetFrame = CGRect.init(x: frame.origin.x-inset.left, y: frame.origin.y-inset.top, width: frame.size.width+inset.left+inset.right, height: frame.size.height+inset.top+inset.bottom)
+        let path = UIBezierPath.init(rect: insetFrame).reversing()
+        self.hollowPaths.append(path)
+        
+        self.updateHollow()
+    }
+    
+    /// 添加圆形镂空范围
+    /// - Parameters:
+    ///   - center: 中心点
+    ///   - radius: 半径
+    public func addHollow(center: CGPoint, radius: CGFloat) {
+        let path = UIBezierPath.init(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: false)
+        self.hollowPaths.append(path)
+        self.updateHollow()
+    }
+    
+    /// 清空所有镂空范围
+    public func clearHollow() {
+        self.hollowPaths.removeAll()
+        self.updateHollow()
+    }
+    
+    /// 更新镂空视图
+    func updateHollow() {
+        let layerPath = UIBezierPath.init(rect: self.bounds)
+        for path in self.hollowPaths {
+            layerPath.append(path)
+        }
+        let maskLayer = CAShapeLayer.init()
+        maskLayer.frame = self.bounds
+        maskLayer.path = layerPath.cgPath
+        
+        self.layer.mask = maskLayer
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        self.updateHollow()
+    }
+    
+    // 拦截点击，做到点击穿透
+    open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        for path in self.hollowPaths {
+            if path.contains(point) {
+                // 传回空代表不处理
+                return nil
+            }
+        }
+        return super.hitTest(point, with: event)
+    }
+    
+}
--- Example/AZLCoverView/ViewController.swift
+++ Example/AZLCoverView/ViewController.swift
@@ -7,17 +7,46 @@
 //
 
 import UIKit
+import AZLCoverView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let button = UIButton.init(frame: CGRect.init(x: 50, y: 50, width: 100, height: 50))
+        button.setTitle("点击我", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.addTarget(self, action: #selector(buttonDidTap(_:)), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+        let button2 = UIButton.init(frame: CGRect.init(x: 50, y: 200, width: 100, height: 50))
+        button2.setTitle("点击我", for: .normal)
+        button2.setTitleColor(UIColor.blue, for: .normal)
+        button2.addTarget(self, action: #selector(buttonDidTap(_:)), for: .touchUpInside)
+        self.view.addSubview(button2)
+        
+        
+        let coverView = AZLCoverView.init(frame: UIScreen.main.bounds)
+        coverView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        coverView.addHollow(frame: button.frame)
+        
+        coverView.addHollow(center: CGPoint.init(x: 200, y: 200), radius: 60)
+        
+        self.view.addSubview(coverView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc
+    func buttonDidTap(_ sender: UIButton) {
+        print("按钮点击了")
     }
 
 }
